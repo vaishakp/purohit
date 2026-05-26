5bd68f81f1c195f309b52f6335591830d44694bc
Vaishak Prasad <vaishakprasad@gmail.com>
2026-05-26 16:03:41 -0700

    init

---
+++ scripts/run_tunnel_manager.sh
@@ -0,0 +1,9 @@
+python scripts/run_tunnel_manager.py \
+  --project-dir /home/vaishak.prasad/Projects/ligo/rean5 \
+  --webdir /home/vaishak.prasad/public_html/monitor \
+  --host 127.0.0.1 \
+  --port 8766 \
+  --token-file /home/vaishak.prasad/Projects/ligo/rean5/control/cgi_mailbox_token.txt \
+  --interval 10 \
+  --plot-interval 300 \
+  --env-mode redacted
