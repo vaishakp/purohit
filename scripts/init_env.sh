99ceb21061a521ec70d232815181ee78ae64e7e6
Vaishak Prasad <vaishakprasad@gmail.com>
2026-05-26 20:31:29 -0400

    sc

--- scripts/init_env.sh
+++ scripts/init_env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-export PUROHIT_REPO=$PWD
-export SOURCE_DIR=/path/to/GWTC5/PE/catalog/config/tree
-export PROJECT_DIR=/path/to/purohit_gwtc5
+export PUROHIT_REPO=${HOME}/Projects/Codes/purohit
+export SOURCE_DIR=/home/pe.o4/GWTC5-HLV
+export PROJECT_DIR=${HOME}/Projects/ligo/gwtc5_purohit
 export WEBDIR=$HOME/public_html/gwtc5_purohit
 export PORT=8766
